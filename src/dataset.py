3eb6c0e435db3989f72cdc397f53420cd0bc0265
gabfssilva <gabfssilva@gmail.com>
2024-05-02 20:41:34 -0300

    Add jupyter notebook

--- src/dataset.py
+++ src/dataset.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-def raw_dataset() -> pd.DataFrame:
-    return pd.read_parquet('../resources/generated/raw_dataset.parquet')
+dataset: pd.DataFrame = (
+    pd.read_parquet('../resources/generated/raw_dataset.parquet'))
 
 # CVDSTRK3 = Have yoy ever had a stroke?
-print(raw_dataset()['CVDSTRK3'].value_counts())
+print(dataset['CVDSTRK3'].value_counts())
 
 print('-' * 50)
 
 # CVDCRHD4 = Have you ever been diagnosed with angina or coronary heart disease?
-print(raw_dataset()['CVDCRHD4'].value_counts())
+print(dataset['CVDCRHD4'].value_counts())
